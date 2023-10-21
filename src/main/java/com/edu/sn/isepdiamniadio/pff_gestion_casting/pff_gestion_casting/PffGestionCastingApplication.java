de8357e4b8caae00d9216143f24ca53b87d67edb
Amina-Goudiaby <a.goudiaby3@isepdiamniadio.edu.sn>
2023-10-21 11:51:00 +0000

    first commit

---
+++ src/main/java/com/edu/sn/isepdiamniadio/pff_gestion_casting/pff_gestion_casting/PffGestionCastingApplication.java
@@ -0,0 +1,13 @@
+package com.edu.sn.isepdiamniadio.pff_gestion_casting.pff_gestion_casting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PffGestionCastingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PffGestionCastingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/sn/isepdiamniadio/pff_gestion_casting/pff_gestion_casting/PffGestionCastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.sn.isepdiamniadio.pff_gestion_casting.pff_gestion_casting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PffGestionCastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
